3381b0c2334317e43f5bdc951cae5e16e7244b52
Fannon <heimlersimon@gmail.com>
2014-06-28 15:13:55 +0200

    * AdvancedTable: More options

--- dist/plastic.js
+++ dist/plastic.js
@@ -3350,13 +3350,28 @@ plastic.modules.display.AdvancedTable = function($el, elAttr) {
                 "type": "boolean",
                 "default": false
             },
+            "lengthMenu": {
+                "description": "Enable Pagination of Table Elements",
+                "type": "array",
+                "default": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ]
+            },
             "searching": {
                 "description": "Enable Searching of Table Elements",
                 "type": "boolean",
                 "default": true
+            },
+            "ordering": {
+                "description": "Feature control ordering (sorting) abilities in DataTables.",
+                "type": "boolean",
+                "default": true
+            },
+            "orderMulti": {
+                "description": "Multiple column ordering ability control.",
+                "type": "boolean",
+                "default": true
             }
         },
-        "additionalProperties": false,
+        "additionalProperties": true,
         "required": []
 
     };
